a33818f7f3a0a4d02056839035dafa0939b364f9
mdfazal <fazalmd964@gmail.com>
2021-05-08 22:57:01 +0530

    Learning Javascript

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+// console.log("Hello world");
+
+// if(1+1 === 2){
+//     console.log("Math still works");
+// }
+
+// 0-5 - free 
+// 5-10 - child $10
+// 10-65 adult $20
+// 65+ senior $10
+
+const age = 80;
+
+if (age < 5){
+    console.log("You are a baby. You get in free");
+} else if (age < 10){
+    console.log("Yu are a child you pay $10");
+}  else if (age < 65){
+    console.log("Yu are a adult you pay $20");
+} else {
+    console.log("You are a senior pay $10");
+}
+
