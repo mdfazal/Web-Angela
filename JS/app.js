8747b153c99e0b6ff555a4601044abace272f5fe
mdfazal <fazalmd964@gmail.com>
2021-05-09 21:08:11 +0530

    Learning Javascript

--- JS/app.js
+++ JS/app.js
@@ -21,3 +21,13 @@ if (age < 5){
     console.log("You are a senior pay $10");
 }
 
+if (age < 5){
+    console.log("You are a baby. You get in free");
+} else if (age < 10){
+    console.log("Yu are a child you pay $10");
+}  else if (age < 65){
+    console.log("Yu are a adult you pay $20");
+} else {
+    console.log("You are a senior pay $10");
+}
+
