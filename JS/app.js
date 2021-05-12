1e8cabb2052e4bf80b827dc532196b4c0ec7c2a6
mdfazal <fazalmd964@gmail.com>
2021-05-12 20:48:54 +0530

    Learning Javascript

--- JS/app.js
+++ JS/app.js
@@ -9,19 +9,25 @@
 // 10-65 adult $20
 // 65+ senior $10
 
-const age = 80;
+// const age = 80;
 
-if (age < 5){
-    console.log("You are a baby. You get in free");
-} else if (age < 10){
-    console.log("Yu are a child you pay $10");
-}  else if (age < 65){
-    console.log("Yu are a adult you pay $20");
-} else {
-    console.log("You are a senior pay $10");
+// if (age < 5){
+//     console.log("You are a baby. You get in free");
+// } else if (age < 10){
+//     console.log("Yu are a child you pay $10");
+// }  else if (age < 65){
+//     console.log("Yu are a adult you pay $20");
+// } else {
+//     console.log("You are a senior pay $10");
+// }
+
+let maximum = parseInt(prompt("Enter the maximum number"));
+while (!maximum){
+    maximum = parseInt(prompt("Enter a valid number"));
 }
 
-if (age > 5){
-    
-}
+const targetNum = Math.floor(Math.random()*maximum) + 1;
+console.log(targetNum);
+
+
 
