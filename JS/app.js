4422236a88217cdcdd9a5700707a644df7f9c037
mdfazal <fazalmd964@gmail.com>
2021-05-12 21:17:05 +0530

    Learning Javascript

--- JS/app.js
+++ JS/app.js
@@ -29,5 +29,24 @@ while (!maximum){
 const targetNum = Math.floor(Math.random()*maximum) + 1;
 console.log(targetNum);
 
+let guess = parseInt(prompt("Enter your first guess"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum){
+    if (guess === 'q') break;
+    attempts++;
+    if(guess > targetNum){
+        guess = prompt("Too high Enter a new Guess:");
+    } else {
+        guess = prompt("Too Low Enter a new guess");
+    }
+}
+
+if(guess === 'q'){
+    console.log("ok, you quit");
+} else {
+    console.log(`You Gor it It took you ${attempts} guesses `);
+}
+
 
 
