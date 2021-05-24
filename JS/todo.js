220ab4098d390351d07655e4c9a15a0e8f502411
mdfazal <fazalmd964@gmail.com>
2021-05-24 20:39:52 +0530

    Learning Javascript

--- JS/todo.js
+++ JS/todo.js
@@ -3,7 +3,17 @@ const todos = ['Task a', 'Task b'];
 while(input !== 'Quit' && input !== 'Q'){
     if(input === 'list'){
         console.log('********************')
+        for(let i =0;i< todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
         console.log('********************')
+    } else if (input === 'new'){
+        const newTodo = prompt('ok what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    } else if (input === 'delete') {
+        const index = prompt ('ok, enter an index to delete');
+        console.log(index);
     }
     input = prompt("What would you like to do?")
 }
