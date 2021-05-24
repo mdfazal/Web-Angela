172806aad9772194458db5ba886cbd69f1e75d76
mdfazal <fazalmd964@gmail.com>
2021-05-24 20:09:14 +0530

    Learning Javascript

--- JS/todo.js
+++ JS/todo.js
@@ -1,1 +1,10 @@
-alert("Hello");
+let input = prompt("What would you like to do?");
+const todos = ['Task a', 'Task b'];
+while(input !== 'Quit' && input !== 'Q'){
+    if(input === 'list'){
+        console.log('********************')
+        console.log('********************')
+    }
+    input = prompt("What would you like to do?")
+}
+console.log("OK quit the app ")
